0d64026c5e12f27c35ec13da20c448c06c737a2d
Marcos Warmling Berti <marcos_wb@outlook.com>
2025-12-03 21:22:59 -0300

    Update training_state.py

--- src/database/training_state.py
+++ src/database/training_state.py
@@ -9,11 +9,10 @@ class TrainingState(Postgres):
 
     def insert_line(self, vocab_id):
         current_date = datetime.now().date()
-        next_day = current_date.replace(day=(current_date.day + 1))
         cursor = self.connection.cursor()
 
         base_sql = f'INSERT INTO {self.schema}.{self.table_name}(vocab_id, streak, confidence, last_review, next_review) VALUES (%s, %s, %s, %s, %s) RETURNING id'
-        cursor.execute(base_sql, (vocab_id, 0, 0, current_date, next_day))
+        cursor.execute(base_sql, (vocab_id, 0, 0, current_date, current_date))
 
         new_id = cursor.fetchone()[0]
         self.connection.commit()
